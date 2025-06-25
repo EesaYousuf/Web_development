dc31e02d83346180bb4ea2c76d774b4769b648b5
eesa <eesayousufyousuf@gmail.com>
2025-06-25 18:55:37 +0530

    js

---
+++ js/js/js/js/js/js/js/js/js/js/js/js/js/js/css/css/css/css/css/javascript/timezone-now.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2)
+const tz = args[0]
+if (!tz) {
+    console.log("Usage: timezone-now.js <time-zone>")
+    process.exit(1)
+}
+try {
+    const now = new Date().toLocaleString("en-US", { timeZone: tz })
+  console.log(`Current time in ${tz}: ${now}`)
+  } catch {
+     console.log("Invalid time zone")
+}
